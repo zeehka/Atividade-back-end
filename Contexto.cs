df72b8952142d5c9dc2461fce0d7af76651e8c24
zeehka <zeka2.jcs@gmail.com>
2023-10-20 20:55:54 -0300

    Atividade back end

---
+++ Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LHpet.Models
+{
+    public class Contexto: DbContext
+    {
+            public Contexto(DbContextOptions<Contexto> options) : base(options)
+            {
+            }
+            public DbSet<Cliente> Cliente { get; set; }
+
+        
+    }
+}
+
---
+++ Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LHpet.Models
+{
+    [Table("Cliente")]
+    public class Cliente
+    {
+        [Key]
+        [Column("Id")]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("Cpf")]
+        [Display(Name = "Cpf")]
+        public string Cpf { get; set; }
+
+        [Column("Email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Column("Paciente")]
+        [Display(Name = "Paciente")]
+        public string Paciente { get; set; }
+    }
+}
